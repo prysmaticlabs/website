701369b0c64c5dfa587a462ae2e709b6a2c688c0
julientregoat <julienctregoat@gmail.com>
2018-04-08 21:54:06 -0400

    getting previous content on page

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ class App extends Component {
     return (
       <div className="app">
         <Header />
-        <p>Body stuff</p>
+        <h3>A team of blockchain engineers committed to scaling Ethereum</h3>
+        <p>We are implementing the first sharding client for the go-ethereum project.</p>
+        <p>Currently, blockchains do not scale. With Ethereum receiving over a million transactions per day, the network is desperate for a solution that will help sustain its massive growth and adoption.</p> 
+		<p>We are building out the first sharding implementation for the Geth (go-ethereum) client, allowing the system to process transactions at over 10x - 100x of its current speed! </p>
       </div>
     );
   }
