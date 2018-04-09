4d8b84062259d520217258bcfc1c59cd7d4471c4
julientregoat <julienctregoat@gmail.com>
2018-04-08 21:27:48 -0400

    starting it off

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Prysm from './Prysm-4.svg'
+
+const Header= () => (
+	<div className="header">
+		<img alt="Prysmatic Labs Logo" className="logo col" src={Prysm} />
+		<h1 className="col">Prysmatic Labs</h1>
+	</div>
+)
+
+export default Header;
