69271abbe4eb03551d50cf294cd3f81c266dcfb8
julientregoat <julienctregoat@gmail.com>
2018-04-10 16:53:57 -0400

    playing with react morph

--- src/Main/MainPage.js
+++ src/Main/MainPage.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import ReactMorph from 'react-morph'
+
 import Jumbo from './Jumbo';
 import IntroContainer from './IntroContainer';
 import TeamContainer from './TeamContainer';
@@ -7,8 +9,22 @@ import TeamContainer from './TeamContainer';
 const MainPage = (props) => {
   return (
     <div>
-        <Jumbo />
-        <IntroContainer />
+    	<ReactMorph>
+    		{({ from, to, fadeIn, go, init, state }) => (
+				<div className="morph-container">
+	    			<div onClick={() => go(1)}>
+	    				<div {...from('comp')}>
+		    				<Jumbo />
+	    				</div>
+	    			</div>
+					<div onClick={() => go(0)}>
+	    				<div {...to('comp')}>
+		    				<IntroContainer />
+	    				</div>
+	    			</div>
+				</div>
+			)}
+    	</ReactMorph>
 		<TeamContainer />
     </div>
   )
