61deda24396a32a45ad15158b653fca73b5cc3a6
julientregoat <julienctregoat@gmail.com>
2018-04-28 19:04:07 -0400

    fix links duh

--- src/FAQ/FAQPage.js
+++ src/FAQ/FAQPage.js
@@ -5,11 +5,11 @@ const FAQPage = (props) => {
     <div className="faq-page">
       <h2 className="yel">FAQ</h2>
       <p><span className="pink">Q:</span> Is this a for-profit venture? </p>
-  		<p><span className="blue">A:</span> No, not at the moment. We are a team of open-source contributors implementing <a className="pink" src="https://github.com/ethereum/wiki/wiki/Sharding-FAQ">sharding</a> for Ethereum because we truly believe the protocol desperately needs this ASAP. As a team, we may consider a for profit venture in the future, but we do not intend to profit directly from our work on sharding.</p>
+  		<p><span className="blue">A:</span> No, not at the moment. We are a team of open-source contributors implementing <a className="pink" href="https://github.com/ethereum/wiki/wiki/Sharding-FAQ">sharding</a> for Ethereum because we truly believe the protocol desperately needs this ASAP. As a team, we may consider a for profit venture in the future, but we do not intend to profit directly from our work on sharding.</p>
   		<p><span className="pink">Q:</span> How can I contribute? </p>
-  		<p><span className="blue">A:</span> Talk to us on <a className="pink" src="https://gitter.im/prysmaticlabs/geth-sharding?source=orgpage">Gitter</a>! We are always looking for more contributors. We'll guide you on how to submit pull requests from there. You can also check our current progress on our Github page.</p>
+  		<p><span className="blue">A:</span> Talk to us on <a className="pink" href="https://gitter.im/prysmaticlabs/geth-sharding?source=orgpage">Gitter</a>! We are always looking for more contributors. We'll guide you on how to submit pull requests from there. You can also check our current progress on our Github page.</p>
   		<p><span className="pink">Q:</span> How can I stay updated? </p>
-  		<p><span className="blue">A:</span> Join our <a className="pink" src="https://rauljordan.us14.list-manage.com/subscribe?u=7bcfcec175d2461e541a3f2a8&id=3a2a17337c">mailing list</a>! We will also be posting major milestones on the <a className="pink" src="https://www.reddit.com/r/ethereum/">/r/ethereum</a> Reddit. We also provide updates on <a className="pink" src="https://www.youtube.com/channel/UCxnN_5jJaU6y--dDQI7qO-Q">YouTube</a> and <a className="pink" src="https://medium.com/prysmatic-labs">Medium</a>.</p>
+  		<p><span className="blue">A:</span> Join our <a className="pink" href="https://rauljordan.us14.list-manage.com/subscribe?u=7bcfcec175d2461e541a3f2a8&id=3a2a17337c">mailing list</a>! We will also be posting major milestones on the <a className="pink" href="https://www.reddit.com/r/ethereum/">/r/ethereum</a> Reddit. We also provide updates on <a className="pink" href="https://www.youtube.com/channel/UCxnN_5jJaU6y--dDQI7qO-Q">YouTube</a> and <a className="pink" href="https://medium.com/prysmatic-labs">Medium</a>.</p>
     </div>
   )
 }
--- src/Main/Intro.js
+++ src/Main/Intro.js
@@ -6,9 +6,9 @@ const Intro = (props) => {
   return (
     <div className="intro">
       <h2>A team of blockchain engineers committed to scaling Ethereum.</h2>
-      <p>We are implementing the first <a className="pink" src="https://github.com/ethereum/wiki/wiki/Sharding-FAQ">sharding</a> client for the go-ethereum project.</p>
+      <p>We are implementing the first <a className="pink" href="https://github.com/ethereum/wiki/wiki/Sharding-FAQ">sharding</a> client for the go-ethereum project.</p>
       <p>Currently, blockchains do not scale. With Ethereum receiving over <i>a million transactions per day</i>, the network is desperate for a solution that will help sustain its massive growth and adoption.</p> 
-  		<p>We are building out the first sharding implementation for the <a className="pink" src="https://github.com/ethereum/go-ethereum">Geth (go-ethereum)</a> client, allowing the system to process transactions at over 10x - 100x of its current speed!</p>
+  		<p>We are building out the first sharding implementation for the <a className="pink" href="https://github.com/ethereum/go-ethereum">Geth (go-ethereum)</a> client, allowing the system to process transactions at over 10x - 100x of its current speed!</p>
   		<div className="subscribe">
         <a href="http://rauljordan.us14.list-manage.com/subscribe?u=7bcfcec175d2461e541a3f2a8&id=3a2a17337c">
           <button className="mail-button">Join Our Mailing List <img src={Email} alt="email icon"/></button>
