e726154d000aa264c52e9a71e18aae2d4241ca8e
julientregoat <julienctregoat@gmail.com>
2018-05-06 15:46:39 -0400

    set up donor assets for donor container

--- src/assets/donors/index.js
+++ src/assets/donors/index.js
@@ -1,5 +1,33 @@
-const Donors = [
+import EthereumLogo from './district0x.svg'
+import ECFLogo from './district0x.svg'
+import district0xLogo from './district0x.svg'
+import SpankChainLogo from './district0x.svg'
+
+const donors = [
   {
-    
-  }
+    logo: EthereumLogo,
+    name: "Ethereum Foundation",
+    website: "https://www.ethereum.org/foundation",
+    twitter: "https://twitter.com/ethereum"
+  },
+  {
+    logo: ECFLogo,
+    name: "Ethereum Community Fund",
+    website: "https://ecf.network/",
+    twitter: "https://twitter.com/ethereumecf"
+  },
+  {
+    logo: district0xLogo,
+    name: "district0x",
+    website: "https://district0x.io/",
+    twitter: "https://twitter.com/district0x"
+  },
+  {
+    logo: SpankChainLogo,
+    name: "SpankChain",
+    website: "https://spankchain.com/",
+    twitter: "https://twitter.com/SpankChain"
+  },
 ]
+
+export default donors
