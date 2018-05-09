b5fc1573166e964149d1af82ab884d78282430bc
julientregoat <julienctregoat@gmail.com>
2018-05-09 00:02:55 -0400

    remove twitter links

--- src/assets/donors/index.js
+++ src/assets/donors/index.js
@@ -7,26 +7,22 @@ const donors = [
   {
     logo: EthereumLogo,
     name: "Ethereum Foundation",
-    website: "https://www.ethereum.org/foundation",
-    twitter: "https://twitter.com/ethereum"
+    website: "https://www.ethereum.org/foundation"
   },
   {
     logo: ECFLogo,
     name: "Ethereum Community Fund",
-    website: "https://ecf.network/",
-    twitter: "https://twitter.com/ethereumecf"
+    website: "https://ecf.network/"
   },
   {
     logo: district0xLogo,
     name: "district0x",
-    website: "https://district0x.io/",
-    twitter: "https://twitter.com/district0x"
+    website: "https://district0x.io/"
   },
   {
     logo: SpankChainLogo,
     name: "SpankChain",
-    website: "https://spankchain.com/",
-    twitter: "https://twitter.com/SpankChain"
+    website: "https://spankchain.com/"
   },
 ]
 
