91c798fff6f6502a50a73d02ae9fc6e35b2d4f96
Preston Van Loon <preston@machinepowered.com>
2018-08-03 23:46:02 -0400

    fix url

--- src/assets/urls.tsx
+++ src/assets/urls.tsx
@@ -1,5 +1,5 @@
 const urls = {
-  sharding: 'https://github.com/ethereum/wiki/wiki/Sharding-FAQ',
+  sharding: 'https://github.com/ethereum/wiki/wiki/Sharding-FAQs',
   gitter: 'https://gitter.im/prysmaticlabs/prysm?source=orgpage',
   mailingList: 'https://rauljordan.us14.list-manage.com/subscribe?u=7bcfcec175d2461e541a3f2a8&id=3a2a17337c',
   rEthereum: 'https://www.reddit.com/r/ethereum/',
