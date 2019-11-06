a1e721e070167160e6ed07769d7beb0e1e50902a
Raul Jordan <raul@prysmaticlabs.com>
2019-11-06 17:20:56 -0600

    include new header

--- src/app/shared/prysmatic-header.component.ts
+++ src/app/shared/prysmatic-header.component.ts
@@ -1,6 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { faMedium, faGitter,
-  faGithub, faTwitter, faDiscord } from '@fortawesome/free-brands-svg-icons';
+import {
+  faDiscord,
+} from '@fortawesome/free-brands-svg-icons';
+
+import {
+  faFire,
+  faUsers,
+  faCodeBranch,
+  faBlog,
+  faNewspaper,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'prysmatic-header',
@@ -8,10 +17,11 @@ import { faMedium, faGitter,
   styleUrls: ['./prysmatic-header.component.css']
 })
 export class PrysmaticHeaderComponent {
-  faMedium = faMedium;
-  faGitter = faGitter;
-  faGithub = faGithub;
-  faTwitter = faTwitter;
+  faFire = faFire;
+  faUsers = faUsers;
+  faCodeBranch = faCodeBranch;
+  faBlog = faBlog;
+  faNewspaper = faNewspaper;
   faDiscord = faDiscord;
 
   menuStatus:boolean = false;
