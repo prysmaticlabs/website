41de7d29c545ac2742c01fee413075d06b98aa93
Raul Jordan <raul@prysmaticlabs.com>
2019-11-08 09:08:00 -0600

    delete unused components

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,15 +1,11 @@
 import {Routes} from '@angular/router';
 import { PrysmaticLabsMasterPageComponent } from './pages/prysmatic-master.page.component';
 import { PrysmaticHomePageComponent} from './pages/home/prysmatic-home.page.component';
-import { PrysmaticAboutUsComponent } from './pages/about-us/prysmatic-about-us.component';
-
 
 export const RouteComponents = [
     PrysmaticLabsMasterPageComponent,
     PrysmaticHomePageComponent,
-    PrysmaticAboutUsComponent,
 ];
 export const routes: Routes =[
     { path: '', component: PrysmaticHomePageComponent },
-    { path: 'about-us', component: PrysmaticAboutUsComponent },
 ];
