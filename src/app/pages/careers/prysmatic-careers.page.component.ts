e135de1c332ccf5f1e4c2a86eb98b37cd8189429
Raul Jordan <raul@prysmaticlabs.com>
2022-03-22 15:07:13 -0400

    hiring

--- src/app/pages/careers/prysmatic-careers.page.component.ts
+++ src/app/pages/careers/prysmatic-careers.page.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { faHome, faFileMedical } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
     selector: 'prysmatic-careers-page',
     templateUrl: './prysmatic-careers.page.component.html',
 })
 export class PrysmaticCareersPageComponent {
+    faHome = faHome;
+    faFileMedical = faFileMedical;
 }
